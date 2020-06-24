bed4c88d266245febbf6b9df70a10db891468d5d
remah massri <remah.massri@gmail.com>
2020-06-24 17:45:53 +0300

    bus mall storage

--- js/app.js
+++ js/app.js
@@ -1,134 +1,216 @@
 'use strict';
-var productsArray = [];
-var imgPathArray = [];
-var nameArray = [];
-var countClickedArray = [];
-var countShownArray = [];
-var previousImgDisplay = [];
-var img1 = document.getElementById('image1');
-var img2 = document.getElementById('image2');
-var img3 = document.getElementById('image3');
-var body = document.getElementsByTagName('body')[0];
-var totalClicks = 0;
 
-function Products(name, filePath){
-  this.name = name;
-  this.filePath = filePath;
-  this.countShown = 0;
-  this.countClicked = 0;
-  productsArray.push(this);
-  imgPathArray.push(filePath);
-  nameArray.push(name);
-};
+//global variables
+var imageOne = document.getElementById('image-one');
+var imageTwo = document.getElementById('image-two');
+var imageThree = document.getElementById('image-three');
+var productElement = document.getElementById('products');
+var button = document.getElementsByTagName('button')[0];
+var allProducts = [];
+var recentlyShown = []; // rolling window of the 5 most recent indexes
+var maxClickedAllowed = 25;
+var namesArray = [];
+var votesArray = [];
+var timesShownArray = [];
+var percentagesArray = [];
 
-var bag = new Products('bag', 'img/bag.jpg');
-var banana = new Products('banana', 'img/banana.jpg');
-var bathroom = new Products('bathroom', 'img/bathroom.jpg');
-var boots = new Products('boots', 'img/boots.jpg');
-var breakfast = new Products('breakfast', 'img/breakfast.jpg');
-var bubblegum = new Products('bubblegum', 'img/bubblegum.jpg');
-var chair = new Products('chair', 'img/chair.jpg');
-var cthulhu = new Products('cthulhu', 'img/cthulhu.jpg');
-var dogDuck = new Products('dog-duck', 'img/dog-duck.jpg');
-var dragon = new Products('dragon', 'img/dragon.jpg');
-var pen = new Products('pen', 'img/pen.jpg');
-var petSweep = new Products('pet-sweep', 'img/pet-sweep.jpg');
-var scissors = new Products('scissors', 'img/scissors.jpg');
-var shark = new Products('shark', 'img/shark.jpg');
-var sweep = new Products('sweep', 'img/sweep.png');
-var tauntaun = new Products('tauntaun', 'img/tauntaun.jpg');
-var unicorn = new Products('unicorn', 'img/unicorn.jpg');
-var usb = new Products('usb', 'img/usb.gif');
-var waterCan = new Products('water-can', 'img/water-can.jpg');
-var wineGlass = new Products('wine-glass', 'img/wine-glass.jpg');
+//constructor function
+function ProductInstances(productName, filepath, numberOfTimesShown=0, numberOfTimesClicked=0) {
+  this.productName = productName;
+  this.filepath = filepath;
+  this.numberOfTimesShown = numberOfTimesShown;
+  this.numberOfTimesClicked = numberOfTimesClicked;
 
-function randomNumGen() {
-  return Math.floor(Math.random() * imgPathArray.length);
-};
+  allProducts.push(this);
+}
 
-function randomImgGen() {
-  var currentImgDisplay = [];
-  while (currentImgDisplay.length < 3){
-    var randomNum = randomNumGen();
-    if (!currentImgDisplay.includes(randomNum) == !previousImgDisplay.includes(randomNum)) {
-      currentImgDisplay.push(randomNum);
-    }
+//get the image Objects
+function getTheImageObjects () {
+  var productInStorage = localStorage.getItem('product');
+
+  if(productInStorage){
+
+    allProducts = JSON.parse(productInStorage);
+
+  } else {
+
+    initalizeDatabase();
+    localStorage.setItem('product', JSON.stringify(allProducts));
   }
-  var prod1 = productsArray[currentImgDisplay[0]];
-  var prod2 = productsArray[currentImgDisplay[1]];
-  var prod3 = productsArray[currentImgDisplay[2]];
-  img1.src = prod1.filePath;
-  img2.src = prod2.filePath;
-  img3.src = prod3.filePath;
-  img1.alt = currentImgDisplay[0];
-  img2.alt = currentImgDisplay[1];
-  img3.alt = currentImgDisplay[2];
-  previousImgDisplay = currentImgDisplay;
-  prod1.countShown++;
-  prod2.countShown++;
-  prod3.countShown++;
-};
-randomImgGen();
+}
 
-function handleClick (){
-  randomImgGen();
-  totalClicks++;
-  var productsArrayIdx = this.alt;
-  productsArray[productsArrayIdx].countClicked++;
-  if (totalClicks >= 25) {
-    img1.removeEventListener('click', handleClick);
-    img2.removeEventListener('click', handleClick);
-    img3.removeEventListener('click', handleClick);
-    var picSection = document.getElementById('imagechoices');
-    body.removeChild(picSection);
-    countClickedArrayPush();
-  }
-};
-img1.addEventListener('click', handleClick);
-img2.addEventListener('click', handleClick);
-img3.addEventListener('click', handleClick);
+// render the object instances
+function initalizeDatabase() {
+  new ProductInstances("R2D2 Bag", "img/bag.jpg");
+  new ProductInstances("Banana Slicer", "img/banana.jpg");
+  new ProductInstances("Bathroom Tablet Holder", "img/bathroom.jpg");
+  new ProductInstances("Open Toed Rainboots", "img/boots.jpg");
+  new ProductInstances("All In One Breakfast Maker", "img/breakfast.jpg");
+  new ProductInstances("Meatball Flavored Bubblegum", "img/bubblegum.jpg");
+  new ProductInstances("Puffy Chair", "img/chair.jpg");
+  new ProductInstances("Cthulhu Action Figure", "img/cthulhu.jpg");
+  new ProductInstances("Duck Beak For Your Dog", "img/dog-duck.jpg");
+  new ProductInstances("Dragon Meat", "img/dragon.jpg");
+  new ProductInstances("Pen Utensils", "img/pen.jpg");
+  new ProductInstances("Pet Friendly Sweeper", "img/pet-sweep.jpg");
+  new ProductInstances("Pizza Scissors", "img/scissors.jpg");
+  new ProductInstances("Shark Sleeping Bag", "img/shark.jpg");
+  new ProductInstances("Baby Sweeper", "img/sweep.png");
+  new ProductInstances("Tauntaun Sleeping Bag", "img/tauntaun.jpg");
+  new ProductInstances("Unicorn Meat", "img/unicorn.jpg");
+  new ProductInstances("Tenticle USB", "img/usb.gif");
+  new ProductInstances("Self Watering Water Can", "img/water-can.jpg");
+  new ProductInstances("Impossible Wine Glass", "img/wine-glass.jpg");
+}
 
-function countClickedArrayPush (){
-  for (var i = 0; i < productsArray.length; i++) {
-    countClickedArray.push(productsArray[i].countClicked);
-    countShownArray.push(productsArray[i].countShown);
+//event listener on parent element for images
+function setUpEventListener () {
+  productElement.addEventListener('click', renderAllFunctions);
+  button.addEventListener('click', clear);
+}
+
+function clear(){
+  localStorage.clear();
+  location.reload();
+}
+
+function removeEventListeners () {
+  productElement.removeEventListener('click', renderAllFunctions);
+}
+
+  function transition() {
+  removeEventListeners();
+
+  var resultsSection = document.getElementById('results-hidden');
+  resultsSection.id = 'results-show';
+  var intro = document.getElementById('intro');
+  intro.className = 'hidden';
+} 
+
+function renderImage(imageElement){
+  var randomNumber = Math.floor(Math.random() * allProducts.length);
+
+  while (
+    recentlyShown.includes(randomNumber)
+  ) {
+    randomNumber = Math.floor(Math.random() * allProducts.length);
   }
-};
-// draw the chart 
-var ctx = document.getElementById('myChart').getContext('2d');
+
+  // add the random number to the most recent five products Shown
+  recentlyShown.push(randomNumber);
+
+  // ensure the products shown only keeps the last five indexes
+  if(recentlyShown.length > 5) {
+    recentlyShown.shift();
+  }
+
+  imageElement.src = allProducts[randomNumber].filepath;
+  imageElement.alt = allProducts[randomNumber].productName;
+  imageElement.productIndex = randomNumber;
+  allProducts[randomNumber].numberOfTimesShown++;
+
+  localStorage.setItem('product', JSON.stringify(allProducts));
+}
+
+//event handler
+function renderAllFunctions() {
+
+  //generaes the final account form
+  if (maxClickedAllowed === 0) {
+    chartArrayGenerator();
+    transition();
+  }
+
+  //counts how many clicks each item gets
+  if (event) {
+    allProducts[event.target.productIndex].numberOfTimesClicked++;
+    maxClickedAllowed--;
+  }
+
+  renderImage(imageOne);
+  renderImage(imageTwo);
+  renderImage(imageThree);
+}
+
+function chartArrayGenerator() {
+
+  for (var i = 0; i < allProducts.length; i++){
+    namesArray.push(allProducts[i].productName);
+    votesArray.push(allProducts[i].numberOfTimesClicked);
+    timesShownArray.push(allProducts[i].numberOfTimesShown);
+    percentagesArray.push((allProducts[i].numberOfTimesClicked/allProducts[i].numberOfTimesShown)*100);
+  }
+}
+
+var ctx = document.getElementById("myChart").getContext('2d');
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
+  type: 'horizontalBar',
+  data: {
+    labels: namesArray,
+    datasets: [{
+      label: '% of votes',
+      data: percentagesArray,
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+        'rgba(255, 99, 132, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255,99,132,1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }]
+  },
+  options: {
+    scales: {
+      yAxes: [{
+        ticks: {
+          beginAtZero:true
         }
+      }]
     }
+  }
 });
+
+
+getTheImageObjects ();
+setUpEventListener();
+renderAllFunctions();
